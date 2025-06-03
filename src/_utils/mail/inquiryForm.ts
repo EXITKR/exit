0178eaebdb811cf999606fef30046e0ec39c032f
HYNOBYTE <hyno0913@naver.com>
2025-06-03 22:59:47 +0900

    mail

---
+++ src/_utils/mail/inquiryForm.ts
@@ -0,0 +1,9 @@
+export const getInquiryForm = (title: string) => {
+  return `
+    <div>
+        <h1>문의사항이 도착했습니다.</h1>
+        <h2>${title}</h2>
+        <p>문의사항을 확인해주세요.</p>
+    </div>
+`;
+};
