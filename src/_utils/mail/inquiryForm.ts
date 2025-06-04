fb33ea8a38dcbf6c4b3ac9e99ccb5b8d92e1f4aa
HYNOBYTE <hyno0913@naver.com>
2025-06-04 14:39:18 +0900

    mail

--- src/_utils/mail/inquiryForm.ts
+++ src/_utils/mail/inquiryForm.ts
@@ -1,8 +1,8 @@
-export const getInquiryForm = (title: string) => {
+export const getInquiryForm = (formData: FormData) => {
   return `
     <div>
         <h1>문의사항이 도착했습니다.</h1>
-        <h2>${title}</h2>
+        <h2></h2>
         <p>문의사항을 확인해주세요.</p>
     </div>
 `;
--- src/_utils/mail/mail.ts
+++ src/_utils/mail/mail.ts
@@ -1,8 +1,9 @@
 "use server"
+import { getInquiryForm } from "@utils/mail/inquiryForm";
 import { getRequestForm } from "@utils/mail/requestForm";
 import nodemailer from "nodemailer";
 
-export const getMailContent = async (html: string, title: string) => {
+export const getMailContent = async (title: string, html: string) => {
   return {
     from: process.env.NEXT_PUBLIC_SMTP_ID,
     to: process.env.NEXT_PUBLIC_CONTACT_EMAIL,
@@ -11,10 +12,18 @@ export const getMailContent = async (html: string, title: string) => {
   };
 };
 
-export const sendContactEmail = async () => {
-  const title = "이게 가능할까?"
-  const mailHtml = await getRequestForm(title)
-  const mailContent = await getMailContent(mailHtml, title)
+export const sendContactEmail = async (formData: FormData, category: string) => {
+  const toEmail: string = formData.get('to_email') as string
+  const toUser: string = formData.get('to_user') as string
+  const toSubject: string = formData.get('to_subject') as string
+  const mailTitle = "[" + category + "] " + toSubject + " - " + toUser + " (" + toEmail + ")"
+  let mailHtml: string = ""
+  if (category === "심사신청") {
+    mailHtml = await getRequestForm(formData) as string
+  } else {
+    mailHtml = await getInquiryForm(formData) as string
+  }
+  const mailContent = await getMailContent(mailTitle, mailHtml)
 
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -27,9 +36,9 @@ export const sendContactEmail = async () => {
   });
 
   try {
-    let info = await transporter.sendMail(mailContent);
-    return { success: true, message: '메일이 성공적으로 전송되었습니다!' };
+    await transporter.sendMail(mailContent);
+    return { success: true, message: '메일이 성공적으로 전송되었습니다!'};
   } catch (error) {
-    return { success: false, message: '메일 전송에 실패했습니다. 잠시 후 다시 시도해주세요.' };
+    return { success: false, message: '메일 전송에 실패했습니다. 잠시 후 다시 시도해주세요. ' + error };
   }
 }
