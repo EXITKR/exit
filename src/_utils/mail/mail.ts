3ba03f456a1d9f1eb93a11fd330c899d5f639b3c
HYNOBYTE <hyno0913@naver.com>
2025-06-04 14:07:15 +0900

    mail

--- src/_utils/mail/mail.ts
+++ src/_utils/mail/mail.ts
@@ -1,16 +1,8 @@
+"use server"
+import { getRequestForm } from "@utils/mail/requestForm";
 import nodemailer from "nodemailer";
 
-export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.NEXT_PUBLIC_SMTP_ID,
-    pass: process.env.NEXT_PUBLIC_SMTP_PW,
-  },
-});
-
-export const getMailContent = (html: string, title: string) => {
+export const getMailContent = async (html: string, title: string) => {
   return {
     from: process.env.NEXT_PUBLIC_SMTP_ID,
     to: process.env.NEXT_PUBLIC_CONTACT_EMAIL,
@@ -18,3 +10,26 @@ export const getMailContent = (html: string, title: string) => {
     html,
   };
 };
+
+export const sendContactEmail = async () => {
+  const title = "이게 가능할까?"
+  const mailHtml = await getRequestForm(title)
+  const mailContent = await getMailContent(mailHtml, title)
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.NEXT_PUBLIC_SMTP_ID,
+      pass: process.env.NEXT_PUBLIC_SMTP_PW,
+    },
+  });
+
+  try {
+    let info = await transporter.sendMail(mailContent);
+    return { success: true, message: '메일이 성공적으로 전송되었습니다!' };
+  } catch (error) {
+    return { success: false, message: '메일 전송에 실패했습니다. 잠시 후 다시 시도해주세요.' };
+  }
+}
