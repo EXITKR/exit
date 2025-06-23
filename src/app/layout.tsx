5d5ef7a3f419579c626d891e9ca3d8e6bd7bd14a
HYNOBYTE <hyno0913@naver.com>
2025-06-23 22:21:44 +0900

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,25 @@ import { Suspense } from "react";
 const config = getConfig()
 
 export const metadata: Metadata = {
-  title: config.company_kr + " | " + config.sub_description,
+  title: config.company_kr + " - " + config.sub_description,
   description: config.description,
+  icons: {
+    icon: "/favicon.ico"
+  },
+  openGraph: {
+    title: config.company_kr + " - " + config.sub_description,
+    description: config.description,
+    url: "https://exitco.kr",
+    images: [
+      {
+        url: "../../assets/imgs/logo/exit_og.png",
+        width: "1200",
+        height: "630",
+        alt: "exit_og"
+      }
+    ],
+    locale: "ko_KR"
+  },
 };
 
 export default function RootLayout({
