3cfc4b0f7234db7cd71c92b566f020e02d0110ea
HYNOBYTE <hyno0913@naver.com>
2025-07-16 14:30:12 +0900

    menu navi modified

---
+++ src/app/overview/page.tsx
@@ -0,0 +1,18 @@
+import ContentBox from "@components/content_ui/Content";
+import { contentDummy } from "@components/contents/ContentDummy";
+import { getMetadata } from "@utils/Metadata";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = getMetadata("회사소개")
+
+export default function Home() {
+  return (
+    <>
+    <div id="content_page">
+      <div className="inner_main">
+        <ContentBox content_box={contentDummy} />
+      </div>
+    </div>
+    </>
+  );
+}
