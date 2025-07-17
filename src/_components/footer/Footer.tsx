b0aa2285e5ff89da9580f5391b4b06cae5dd827e
HYNOBYTE <hyno0913@naver.com>
2025-07-17 20:07:59 +0900

    footer renewal

--- src/_components/footer/Footer.tsx
+++ src/_components/footer/Footer.tsx
@@ -14,31 +14,7 @@ const Footer = () => {
       <footer id="footer_pc">
         <div className="inner_main footer_main">
           <div className="top_section">
-            <div className="info_section">
-              <span className="company_title" onClick={() => router.push("/")}>{config.company_en}</span>
-              <div className="info_content">
-                <span className="company_name">{config.company_kr}</span>
-                <span className="company_ceo">대표이사 :: {config.ceo}</span>
-                <span className="company_ceo">개인정보관리 책임자 :: {config.duty}</span>
-                <span className="company_number">사업자 등록 번호 :: {config.bs_number}</span>
-                <span className="company_address">주소 :: {config.address}</span>
-                <span className="company_tel">Tel :: {config.tel_number}</span>
-                <span className="company_fax">Fax :: {config.fax_number}</span>
-                <span className="company_mail">e-mail :: {config.email}</span>
-              </div>
-            </div>
-            {/* <div className="menu_section">
-              {navigationList.map((val, idx) => (
-                <div className={"menu menu_" + (idx + 1).toString().padStart(2, '0')} key={idx}>
-                  <span className="menu_title" onClick={() => router.push(val.url)}>{val.name}</span>
-                  {val.sub.map((item, index) => (
-                    <span className="menu_list" onClick={() => router.push(item.url)} key={index}>{item.name}</span>
-                  ))}
-                </div>
-              ))}
-            </div> */}
-          </div>
-          <div className="bot_section">
+            <span className="company_title" onClick={() => router.push("/")}>{config.company_en}</span>
             <div className="contact_section">
               <Link href={config.kakao} target="blank">
                 <div className="icon_circle">
@@ -58,12 +34,31 @@ const Footer = () => {
               <ActiveBtn style="white ctr_mb" name="문의하기" onClick={() => router.push("/contact_us?index=0")} disabled={false} />
               <ActiveBtn style="white ctr_mb" name="인증 신청하기" onClick={() => router.push("/contact_us?index=1")} disabled={false} />
             </div>
+          </div>
+          <div className="bot_section">
+            <div className="info_section">
+              <div className="info_content">
+                <span className="company_name">{config.company_kr}</span>
+                <span className="company_ceo">대표이사 & 개인정보관리 책임자 :: {config.ceo}</span>
+                <span className="company_number">사업자 등록 번호 :: {config.bs_number}</span>
+              </div>
+              <div className="info_content">
+                <span className="company_tel">Tel :: {config.tel_number}</span>
+                <span className="company_fax">Fax :: {config.fax_number}</span>
+                <span className="company_mail">e-mail :: {config.email}</span>
+              </div>
+              <div className="info_content">
+                <span className="company_address">{config.address}</span>
+              </div>
+              <div className="info_content">
+                <span className="copyright">&copy; 2025 {config.company_en} All rights reserved.</span>
+              </div>
+            </div>
             <div className="copy_section">
               <div className="terms_section">
                 <span className="terms" onClick={() => router.push("/terms_of_use")}>이용약관</span>
                 <span className="terms" onClick={() => router.push("/privacy_policy")}>개인정보처리방침</span>
               </div>
-              <span className="copyright">&copy; 2025 {config.company_en} All rights reserved.</span>
             </div>
           </div>
         </div>
