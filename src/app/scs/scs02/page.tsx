1e84de3c1457c47025f29375cf2eebadc3655b3a
HYNOBYTE <hyno0913@naver.com>
2025-07-23 14:50:57 +0900

    content box renewal

--- src/app/scs/scs02/page.tsx
+++ src/app/scs/scs02/page.tsx
@@ -17,26 +17,26 @@ export default function Home() {
       case 0:
         return <>
           <div className="content_box">
-            <div className="content_box_content title">
+            <div className="content_box_content title gc-1-4">
               <span className="content_text">
                 <span className="fw-500 tx-30">
                   국내 여러 기업들 및 여러 공공기관 등 개인정보 유출 사고 증가하며,
-                  <br/>
+                  <br />
                   심화되는 개인 정보 보호 유출 위험을 예방하기 위한 개인정보보호 시스템과 프로세스의 구축이 요구되고 있습니다.
-                  </span>
+                </span>
               </span>
             </div>
-            <div className="content_box_content desc_1 sect_point">
+            <div className="content_box_content gc-1-2 sect_point">
               <span className="content_text">
                 이미 글로벌 유통사 및 대형 유통사들은 본사뿐만이 아닌 물류를 위탁한 3PL 업체들을 대상으로도 개인정보관리 프로세스를 적용하고 있는 추세입니다.
               </span>
             </div>
-            <div className="content_box_content desc_1 sect_point">
+            <div className="content_box_content gc-1-2 sect_point">
               <span className="content_text">
                 특히, 배송 및 이벤트 행사 등의 진행에 있어 대량의 개인정보의 유출은 기업의 이미지 저하, 개인정보보호법 70조-75조 사이의 양형 기준에 근거해 법적 처벌 대상이 될 수 있습니다.
               </span>
             </div>
-            <div className="content_box_content desc_3 sect_point">
+            <div className="content_box_content gc-2-4 gr-2-4 sect_point">
               <span className="content_text">
                 ‘유통사 - 납품업체 - 3PL 업체’의 흐름으로 이어지는 전방위적인 개인정보보호 시스템 구축은 개인정보보호법 위반에 대한 기업의 법적 리스크를 최소화하고 안전하게 개인정보를 활용하기 위한 시스템 구축을 적극적으로 도입하고 있습니다.
               </span>
@@ -46,23 +46,23 @@ export default function Home() {
       case 1:
         return <>
           <div className="content_box">
-            <div className="content_box_content title">
+            <div className="content_box_content title gc-1-4">
               <span className="content_text">
                 <span className="fw-500 tx-30">
                   정보유출방지(DLP)는 조직 내 중요 정보와 데이터를 보호하기 위해,
-                  <br/>
+                  <br />
                   문서·이미지 등 저장된 파일 내 주요 정보를 탐지하고 저장매체, 출력, 파일 전송 등
-                  <br/>
+                  <br />
                   다양한 경로를 통해 발생할 수 있는 유출을 차단함으로써 외부 유출을 예방하는 시스템입니다.
                 </span>
               </span>
             </div>
-            <div className="content_box_content desc_1 sect_point">
+            <div className="content_box_content gc-1-2 sect_point">
               <span className="content_text">
                 DLP 시스템은 이메일, 웹하드, 메신저 등 다양한 경로를 통한 정보 유출을 효과적으로 차단할 수 있습니다.
               </span>
             </div>
-            <div className="content_box_content desc_2 sect_point">
+            <div className="content_box_content gc-2-4 sect_point">
               <span className="content_text">
                 또한 설치와 관리가 용이하고, 사내 네트워크를 통한 데이터 전송을 집중적으로 감시할 수 있어 기업들이 필수적으로 도입하고 있습니다.
               </span>
@@ -72,19 +72,19 @@ export default function Home() {
       case 2:
         return <>
           <div className="content_box">
-            <div className="content_box_content title">
+            <div className="content_box_content title gc-1-4">
               <span className="content_text">
                 <span className="fw-500 tx-30">
                   ISO 27001은 규모와 활동분야에 관계없이 모든 기업에 정보보안관리시스템을 구축, 구현, 유지 및 지속적으로 개선하기 위한 국제표준입니다.
                 </span>
               </span>
             </div>
-            <div className="content_box_content desc_1 sect_point">
+            <div className="content_box_content gc-1-2 sect_point">
               <span className="content_text">
                 기업의 조직이 소유하거나 처리하는 데이터의 보안과 관련된 위험을 관리하는 시스템을 구축하여 정보보안 리스크를 관리하고 있음을 입증하는데 사용됩니다.
               </span>
             </div>
-            <div className="content_box_content desc_2 sect_point">
+            <div className="content_box_content gc-2-4 sect_point">
               <span className="content_text">
                 ISO 27001은 사이버 범죄가 증가하고 새로운 위협이 끊임없이 등장함에 따라 조직이 위험을 인식하고 취약점을 사전에 파악하여 해결할 수 있도록 지원합니다.
               </span>
@@ -94,26 +94,26 @@ export default function Home() {
       default:
         return <>
           <div className="content_box">
-            <div className="content_box_content title">
+            <div className="content_box_content title gc-1-4">
               <span className="content_text">
                 <span className="fw-500 tx-30">
                   국내 여러 기업들 및 여러 공공기관 등 개인정보 유출 사고 증가하며,
-                  <br/>
+                  <br />
                   심화되는 개인 정보 보호 유출 위험을 예방하기 위한 개인정보보호 시스템과 프로세스의 구축이 요구되고 있습니다.
-                  </span>
+                </span>
               </span>
             </div>
-            <div className="content_box_content desc_1 sect_point">
+            <div className="content_box_content gc-1-2 sect_point">
               <span className="content_text">
                 이미 글로벌 유통사 및 대형 유통사들은 본사뿐만이 아닌 물류를 위탁한 3PL 업체들을 대상으로도 개인정보관리 프로세스를 적용하고 있는 추세입니다.
               </span>
             </div>
-            <div className="content_box_content desc_1 sect_point">
+            <div className="content_box_content gc-1-2 sect_point">
               <span className="content_text">
                 특히, 배송 및 이벤트 행사 등의 진행에 있어 대량의 개인정보의 유출은 기업의 이미지 저하, 개인정보보호법 70조-75조 사이의 양형 기준에 근거해 법적 처벌 대상이 될 수 있습니다.
               </span>
             </div>
-            <div className="content_box_content desc_3 sect_point">
+            <div className="content_box_content gc-2-4 gr-2-4 sect_point">
               <span className="content_text">
                 ‘유통사 - 납품업체 - 3PL 업체’의 흐름으로 이어지는 전방위적인 개인정보보호 시스템 구축은 개인정보보호법 위반에 대한 기업의 법적 리스크를 최소화하고 안전하게 개인정보를 활용하기 위한 시스템 구축을 적극적으로 도입하고 있습니다.
               </span>
--- src/app/scs/scs06/page.tsx
+++ src/app/scs/scs06/page.tsx
@@ -12,22 +12,22 @@ export default function Home() {
         <Title title="COC (Code Of Conduct)" desc="Customized SCS (Supply Chain Security)" />
         <div className="inner_main page_inner">
           <div className="content_box">
-            <div className="content_box_content title">
+            <div className="content_box_content title gc-1-4">
               <span className="content_text">
                 <span className="fw-500 tx-30">COC는 조직이나 기업이 구성원들에게 기대하는 행동 기준을 명확히 정의하는 규범입니다.</span>
               </span>
             </div>
-            <div className="content_box_content desc_1 sect_point">
+            <div className="content_box_content gc-1-2 sect_point">
               <span className="content_text">
                 공급업체를 위한 행동강령(COC)은 아동노동, 강제노동, 차별 금지 등 국제노동기준의 준수 여부를 포함한 노동·인권 관련 항목과 함께, 작업장 안전, 법정 근로 조건, 복리후생 이행 등 현장 관리 실태 전반을 종합적으로 평가하는 기준입니다.
               </span>
             </div>
-            <div className="content_box_content desc_1 sect_point">
+            <div className="content_box_content gc-1-2 sect_point">
               <span className="content_text">
                 최근의 COC는 단일 공급업체의 준법 여부를 넘어서, 전체 공급망의 윤리성과 지속 가능성을 체계적으로 관리하는 방향으로 진화하고 있습니다.
               </span>
             </div>
-            <div className="content_box_content desc_3 sect_point">
+            <div className="content_box_content gc-2-4 gr-2-4 sect_point">
               <span className="content_text">
                 COC는 조직이 투명하고 공정한 방식으로 운영되도록 보장하며, 규범을 준수함으로써 법적 리스크를 감소시키고, 구성원들이 공통된 가치와 행동 기준을 공유하여 긍정적인 조직문화를 형성하며, 윤리적 문제나 내부 갈등 발생 시 명확한 기준을 제공하여 효과적으로 대응할 수 있습니다.
               </span>
