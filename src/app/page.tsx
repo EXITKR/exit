20bd53450a2373f387d4aea9d07c44717fca2180
HYNOBYTE <hyno0913@naver.com>
2025-08-06 14:45:19 +0900

    1. 홈페이지    -  EXIT 슬로건의 어순에 맞춰서(사선으로) 배열 수정    - 홈페이지를 열었을때 슬로건이 한번 보이도록 올려주세요    - 슬로건과 같이있던 overview, contact us 버튼 및 구분선 삭제    - our client 베너 주석처리    - 푸터 문의하기 와 인증 신청하기  Contact Us 일원화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,11 +15,11 @@ export default function Home() {
         <div className="main_section">
           <div className="inner_main main_main">
             <div className="slogan_section">
-              <span className="slogan"><span className="emph">EX</span>press</span>
-              <span className="slogan"><span className="emph">I</span>nfallible</span>
-              <span className="slogan"><span className="emph">T</span>rustworthy</span>
+              <span className="slogan"><span className="emph">EX</span><span className="sub">press</span></span>
+              <span className="slogan"><span className="emph inv">EX</span><span className="emph">I</span><span className="sub">nfallible</span></span>
+              <span className="slogan"><span className="emph inv">EXI</span><span className="emph">T</span><span className="sub">rustworthy</span></span>
             </div>
-            <div className="button_section">
+            {/* <div className="button_section">
               <div className="button_tab" onClick={() => router.push("/overview")}>
                 <FaBuilding className="btn_icon"/>
                 <span className="btn_text">OVERVIEW</span>
@@ -28,11 +28,11 @@ export default function Home() {
                 <IoDocumentText className="btn_icon"/>
                 <span className="btn_text">CONTACT US</span>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
 
-        <div className="trust_section">
+        {/* <div className="trust_section">
           <Title title="OUR CLIENTS" desc="엑시트를 신뢰하는 고객기업 입니다."/>
           <div className="inner_main trust_main">
             <div className={"trust_list " + (clientList.length < 5 ? ("grid_" + clientList.length) : "grid_5")}>
@@ -43,7 +43,7 @@ export default function Home() {
               ))}
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
