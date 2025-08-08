a54ee0d1e5067e4cc135a2bbba29fccb025d1cbe
HYNOBYTE <hyno0913@naver.com>
2025-08-08 23:18:15 +0900

    robots.txt & sitemap.xml

---
+++ src/app/sitemap.ts
@@ -0,0 +1,54 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: 'https://www.exitco.kr',
+        },
+        {
+            url: 'https://www.exitco.kr/contact_us',
+        },
+        {
+            url: 'https://www.exitco.kr/overview',
+        },
+        {
+            url: 'https://www.exitco.kr/qesg/qesg01',
+        },
+        {
+            url: 'https://www.exitco.kr/qesg/qesg02',
+        },
+        {
+            url: 'https://www.exitco.kr/qesg/qesg03',
+        },
+        {
+            url: 'https://www.exitco.kr/qesg/qesg04',
+        },
+        {
+            url: 'https://www.exitco.kr/qesg/qesg05',
+        },
+        {
+            url: 'https://www.exitco.kr/scs/scs01',
+        },
+        {
+            url: 'https://www.exitco.kr/scs/scs02',
+        },
+        {
+            url: 'https://www.exitco.kr/scs/scs05',
+        },
+        {
+            url: 'https://www.exitco.kr/scs/scs06',
+        },
+        {
+            url: 'https://www.exitco.kr/iso/iso01',
+        },
+        {
+            url: 'https://www.exitco.kr/iso/iso02',
+        },
+        {
+            url: 'https://www.exitco.kr/iso/iso03',
+        },
+        {
+            url: 'https://www.exitco.kr/iso/iso04',
+        },
+    ]
+}
